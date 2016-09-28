d4ce060cb9453a890d21fce09d0b2a1f55d7b422
LenaKor <kor_helen@yahoo.com>
2016-09-28 17:23:14 +0300

    added tables

--- Clinic.sql
+++ Clinic.sql
@@ -64,16 +64,20 @@ DROP TABLE Doctors;
 CREATE TABLE dbo.Doctors
 (
 DocId int not null IDENTITY(1,1) primary key,
-DocName VARCHAR(64) NOT NULL,
+DocName VARCHAR(128) NOT NULL,
+DocRank VARCHAR(128),
 CabinetId int not null FOREIGN KEY REFERENCES dbo.Cabinets(CabinetId),
 DocSpecialisation VARCHAR(64) NOT NULL,
+
 );
 GO
 SET IDENTITY_INSERT Doctors ON;
-INSERT INTO Doctors(DocId, DocName,CabinetId, DocSpecialisation)
-VALUES( 1,'Ivanov I.I.', 1, 'Plastic Surgeon');
-INSERT INTO Doctors(DocId, DocName,CabinetId, DocSpecialisation)
-VALUES( 2,'Than Xao', 2, 'Massage Therapist');
+INSERT INTO Doctors(DocId, DocName,DocRank, CabinetId, DocSpecialisation)
+VALUES( 1,'Ivanov I.I.', 'Professor', 1, 'Plastic Surgeon');
+INSERT INTO Doctors(DocId, DocName,DocRank, CabinetId, DocSpecialisation)
+VALUES( 2,'Than Xao',  'Assistant Nurse', 2, 'Massage Therapist');
+INSERT INTO Doctors(DocId, DocName,DocRank, CabinetId, DocSpecialisation)
+VALUES( 3,'Abrikosov A.A.', 'Head Chief of Medicine', 1, 'Dermogastrotomy');
 SET IDENTITY_INSERT Doctors OFF;
 GO
 
@@ -94,6 +98,8 @@ GO
 SET IDENTITY_INSERT Procedures ON;
 INSERT INTO Procedures(ProcId, DocId, ProcName, DateStart, DateFinish, ProcDescription,ProcCost)
 VALUES( 1,2,'Massage',  '2016-09-09 10:00:00', '2016-09-09 10:59:00', 'Aroma massage', 55.99);
+INSERT INTO Procedures(ProcId, DocId, ProcName, DateStart, DateFinish, ProcDescription,ProcCost)
+VALUES( 2,2,'Laser Hair Removal',  '2016-09-09 10:00:00', '2016-09-09 10:59:00', 'Affordable laser hair removal for men and women', 10.99);
 SET IDENTITY_INSERT Procedures OFF;
 GO
 ---------Clients-----------
@@ -103,15 +109,37 @@ CREATE TABLE dbo.Clients
 (
 ClientId int not null IDENTITY(1,1) primary key,
 ClientName VARCHAR(128) NOT NULL,
+ClientLastName VARCHAR(128) NOT NULL,
 ClientAddress VARCHAR(64) NOT NULL,
 ClientDataBirthday Date NOT NULL,
-
-
+ClientPhone VARCHAR(64),
+ClientEmail VARCHAR(64)
 );
 GO
 SET IDENTITY_INSERT Clients ON;
-INSERT INTO Clients(ClientId, ClientName, ClientAddress, ClientDataBirthday)
-VALUES( 1,'Romashova Alina ', 'Mechnikova 8, kv.16', '1963-09-07');
+INSERT INTO Clients(ClientId, ClientName,ClientLastName, ClientAddress, ClientDataBirthday,ClientPhone, ClientEmail )
+VALUES( 1, 'Alina', 'Romashova','Mechnikova 8, kv.16', '1963-09-07', '555-0256', 'ght@yahoo.com');
+INSERT INTO Clients(ClientId, ClientName,ClientLastName, ClientAddress, ClientDataBirthday,ClientPhone, ClientEmail)
+VALUES( 2, 'Iryna', 'Belokurova','Belogorodskaya vul, bud 57d, kv.90', '1991-12-12', '666-9875', 'belokurova@gmail.com');
+INSERT INTO Clients(ClientId, ClientName,ClientLastName, ClientAddress, ClientDataBirthday,ClientPhone, ClientEmail)
+VALUES( 3, 'Natalia', 'Kovalchuck','Komarova prospekt 14, kv.15', '2000-01-02', '224-1900', 'natkoval@gmail.com');
+INSERT INTO Clients(ClientId, ClientName,ClientLastName, ClientAddress, ClientDataBirthday,ClientPhone, ClientEmail)
+VALUES( 4, 'Rebecca', 'Klark','Chernobylskaya 6, kv.27', '1999-11-10', '066-8614', 'klark@ukr.net');
+INSERT INTO Clients(ClientId, ClientName,ClientLastName, ClientAddress, ClientDataBirthday,ClientPhone, ClientEmail)
+VALUES( 5, 'Olga', 'Taran','1 Maya, bud.4, kv.9', '1980-03-05', '781-8745', 'manager@microsoft.com');
+INSERT INTO Clients(ClientId, ClientName,ClientLastName, ClientAddress, ClientDataBirthday,ClientPhone, ClientEmail)
+VALUES( 6, 'Malvina', 'Mamedova','Schevchenko bulvar 53, kv.2', '1975-05-03', '148-1890', 'mamed@yandex.ru');
+INSERT INTO Clients(ClientId, ClientName,ClientLastName, ClientAddress, ClientDataBirthday,ClientPhone, ClientEmail)
+VALUES( 7, 'Lyubov', 'Sandugei','Lyuteranskaya 11 k.3, kv.19', '1981-12-01', '128-8549', 'sunflower@flower.com');
+INSERT INTO Clients(ClientId, ClientName,ClientLastName, ClientAddress, ClientDataBirthday,ClientPhone, ClientEmail)
+VALUES( 8, 'Zemfira', 'Ramazanova','Trehsvyatitelskaya 1, kv.5', '1978-01-01', '333-3578', 'zemfira@google.com');
+INSERT INTO Clients(ClientId, ClientName,ClientLastName, ClientAddress, ClientDataBirthday,ClientPhone, ClientEmail)
+VALUES( 9, 'Yanina', 'Petrovich','Pobedy prospekt 134, kv.20', '1969-10-10', '190-3308', 'topmanager@itstep.com');
+INSERT INTO Clients(ClientId, ClientName,ClientLastName, ClientAddress, ClientDataBirthday,ClientPhone, ClientEmail)
+VALUES( 10, 'Tereza', 'Yankovskaya','Bankovskaya 31, kv.50a', '1977-03-03', '460-7311', 'jdhj@yankovskaya.com');
+INSERT INTO Clients(ClientId, ClientName,ClientLastName, ClientAddress, ClientDataBirthday,ClientPhone, ClientEmail)
+VALUES( 11, 'Snezhana', 'Babenko','Sedova vul, bud.1', '1955-02-06', '980-6209', 'babenko@babenko.ua');
+
 
 SET IDENTITY_INSERT Clients OFF;
 GO
@@ -134,4 +162,15 @@ INSERT INTO Schedule(ScheduleId,ScheduleDay, ScheduleHour, ScheduleMinute)
 VALUES( 1,'Monday', 2, 30);
 
 SET IDENTITY_INSERT Schedule OFF;
+GO
+---------Appointment-----------
+IF OBJECT_ID('Appointment', 'U') IS NOT NULL
+DROP TABLE Appointment;
+CREATE TABLE dbo.Appointment
+(
+AppointmentId int not null IDENTITY(1,1) primary key,
+ClientId int not null FOREIGN KEY REFERENCES dbo.Clients(ClientId),
+
+
+);
 GO

243c3d4afa8d5ea40f31208b4a81f4abb293371a
ivoshkulat <igor.voshkulat@gmail.com>
2016-09-29 11:21:53 +0300

    Add MVVM part 1

---
+++ Clinic/View/Make_Appointment.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Clinic.View
+{
+    /// <summary>
+    /// Interaction logic for Make_Appointment.xaml
+    /// </summary>
+    public partial class Make_Appointment : Window
+    {
+        public Make_Appointment()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
---
+++ Clinic/ViewModel/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+        public virtual void OnDispose() { }
+    }
+}
